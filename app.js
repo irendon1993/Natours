fc5e531e56b4339d5cbce698725b8489769a034c
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-23 18:33:41 -0500

    Setting up enviroment variables

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
