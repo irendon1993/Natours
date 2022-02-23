ce95278de2e6317dad1bcda3fc54d6ee1ecd3ed0
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-23 12:53:38 -0500

    Creating and Mounting Routers

--- app.js
+++ app.js
@@ -142,24 +142,19 @@ const deleteUser = (req, res) => {
   });
 };
 // Routes
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-// app.get('/api/v1/tours', getTour);
-// app.get('/api/v1/tours/:id', getAllTours);
-// // app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-app.route('/api/v1/users:id').get(getUser).patch(updateUser).delete(deleteUser);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // Server
 const port = 3000;
